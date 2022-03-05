bdd693e14ea3a0af06e4fea79a5ac8dd66c5ab29
KaluMiracle <60604821+KaluMiracle@users.noreply.github.com>
2022-03-05 11:07:19 +0100

    error handling

--- pages/api/fetchWeather.js
+++ pages/api/fetchWeather.js
@@ -5,13 +5,21 @@ const URL = 'https://api.openweathermap.org/data/2.5/weather'
 const API_KEY = '670763202e5a156f0432a7d74336c489'
 
 export default async function fetchWeather(query) {
-  const {data} = await axios.get(URL,{
-    params: {
-      q: query,
-      units: "metric",
-      APPID: API_KEY
-    }
-  });
+  try{
+    console.log("fetching data")
+    const {data} = await axios.get(URL,{
+      params: {
+        q: query,
+        units: "metric",
+        APPID: API_KEY
+      }
+    });
 
-  return data
+    return data;
+  }
+  catch(error){
+    console.log("errorrr: " + error)
+    return "can't fetch weather info at the moment"
+  }
+
 }
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,37 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { useState } from 'react'
+import React from 'react'
 import fetchWeather from './api/fetchWeather'
 import bg from '../assets/img/bg.jpg'
 import styles from "./index.module.scss";
+import { useEffect } from 'react/cjs/react.production.min'
 
 export default function Home() {
 
   const [query, setQuery]= useState('');
-  const [weather, setWeather] = useState({})
-  const [cityInfo, setCityInfo] = useState({})
-  const [cityInfoMain, setCityInfoMain] = useState({})
+  const [weather, setWeather] = useState({weather: "",cityInfoc: '',cityInfoMain: ''})
+ 
 
   const search = async (e) =>{
     if(e.key === 'Enter'){
       const data = await fetchWeather(query);
       console.log(data)
-      setCityInfo(data)
-      setWeather(data.weather[0]);
-      setCityInfoMain(data.main);
-
-      console.log(data.weather[0])
-      setQuery('')
+      if (data.weather){
+        setWeather({
+          description:  data.weather[0].description,
+          name: data.name,
+          country: data.sys.country,
+          temp: data.main.temp,
+          error: "",
+        },)  
+      }else{
+        setWeather({error: "can't fetch weather infomation check network connection and city name"})
+      }
+      
     }
   }
+
   return (
     <div className={styles.index}>
       <Head>
@@ -46,22 +54,28 @@ export default function Home() {
         onKeyPress={search}
       />
 
+
       <div className={styles.weatherCard}> 
         <h2 className={styles.cityInfo}>
-          {cityInfo.name}
+          {weather.name}
           
         </h2>
+        <p>{weather.country}</p>
         <h2 className={styles.temp}>
-          {cityInfoMain.temp}
+          {weather.temp}
           
         </h2>
+
         
         <h2 className={styles.description}>
           {weather.description}
           
         </h2>
+
+        <h2 className={styles.error}>{weather.error}</h2>
       
       </div>
+
     </div>
   )
 }
