176b7e25d90c0d66b4e4ba8eea8ac0434308210a
KaluMiracle <60604821+KaluMiracle@users.noreply.github.com>
2022-03-07 11:36:10 +0100

    weather icon

--- pages/index.js
+++ pages/index.js
@@ -11,40 +11,40 @@ export default function Home() {
   const [query, setQuery]= useState('');
   const [weather, setWeather] = useState({})
   const [date, setDate] = useState("")
+  const [icon, setIcon] = React.useState("")
 
   
   const getDate  = () => {
-    console.log("getting date")
-
+    console.log("getting date");
     const dateObj = new Date();
-    if (weather.description){
-      setDate(dateObj.toDateString());
-    }else{
-      setDate("")
-    }
-    return date;
-    
+    weather.description ? setDate(dateObj.toDateString()) : setDate("");
+  } 
+
+  const getIcon  = () => {
+    console.log("getting icon");
+    weather.description ? setIcon(`https://openweathermap.org/img/wn/${weather.icon}@2x.png`) : setIcon("");
   } 
 
   React.useEffect(()=>{
     getDate();
+    getIcon();
   }, [weather])
 
   const search = async (e) =>{
     if(e.key === 'Enter'){
       const data = await fetchWeather(query);
       console.log(data)
-      if (data.weather){
-        setWeather({
+      data.weather ? setWeather({
           description:  data.weather[0].description,
           name: data.name,
           country: data.sys.country,
           temp: data.main.temp,
           error: "",
-        },)  
-      }else{
-        setWeather({error: "can't fetch weather infomation check network connection and city name"})
-      }
+          icon: data.weather[0].icon
+        },)  : setWeather({
+          error: "can't fetch weather infomation check network connection and city name"
+        })
+      
       
     }
   }
@@ -82,19 +82,29 @@ export default function Home() {
           <p className={styles.country}>{weather.country}</p>
         </div>
         
-        <p className={styles.date}>
+        <p className={styles.date} style={{
+          color: "black"
+        }}>
           {date}
         </p>
-        <p className={styles.temp}>
-          {weather.temp }
-          <sup>{ date == "" ? "" : "o"}</sup>
+        <p className={styles.temp} style={{
+          display: weather.temp? "flex": "none",
+          ...styles
+          
+        }} >
+          {Math.round(weather.temp)  }
+          <sup> &deg;C</sup>
           
         </p>
+
+        <img  src={icon} alt=""/>
 
         
         <p className={styles.description}>
           {weather.description}
         </p>
+
+
 
         <p className={styles.error}>{weather.error}</p>
       
